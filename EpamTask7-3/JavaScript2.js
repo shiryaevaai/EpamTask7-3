4fa3dfb0d972a6443f0b9f6f742fb4df8602fd6b
User <shiryaevaai92@gmail.com>
2014-05-08 10:49:10 +0300

    First

---
+++ EpamTask7-3/JavaScript2.js
@@ -0,0 +1,113 @@
+﻿
+function AllToSelected(listID) {
+    var list = document.getElementById(listID),
+     avList = list.getElementsByClassName("available")[0],
+     selList = list.getElementsByClassName("selected")[0],    
+     children = avList.children,
+     i,
+     option;
+
+    for (i = 0; i < children.length; i++) {
+        option = document.createElement('li');
+        option.innerHTML = children[i].innerHTML;
+        option.onclick = children[i].onclick;
+        option.className = children[i].className;
+        option.id = children[i].id;
+            
+        selList.appendChild(option); 
+    }
+
+        avList.innerHTML = "";    
+}
+
+function AllToAvailable(listID) {
+    var list = document.getElementById(listID),
+     avList = list.getElementsByClassName("available")[0],
+     selList = list.getElementsByClassName("selected")[0],
+   
+     children = selList.children,
+     i,
+     option;
+
+    for (i = 0; i < children.length; i++) {
+        option = document.createElement('li');
+        option.innerHTML = children[i].innerHTML;
+        option.onclick = children[i].onclick;
+        option.className = children[i].className;
+        option.id = children[i].id;
+            
+        avList.appendChild(option);
+    }
+
+    selList.innerHTML = "";    
+}
+
+function ToSelected(listID) {
+    var list = document.getElementById(listID),
+     avList = list.getElementsByClassName("available")[0],
+     selList = list.getElementsByClassName("selected")[0],
+
+     marked = false,
+
+    children = avList.children;
+
+    for (var i = 0; i < children.length; i++) {
+
+        if (children[i].className.indexOf("marked") > -1) {
+            children[i].className = children[i].className.replace(/(?:^|\s)marked(?!\S)/, '');
+            selList.appendChild(children[i]);
+            //   avList.removeChild(children[i]); ???
+            marked = true;
+            i--;
+        }
+    }
+
+    if (!marked) {
+        alert("Ничего не выделено");
+    }
+}
+
+function ToAvailable(listID) {
+    var list = document.getElementById(listID),
+     avList = list.getElementsByClassName("available")[0],
+     selList = list.getElementsByClassName("selected")[0],
+
+     marked = false,
+     children = selList.children,
+     i;
+
+    for (i = 0; i < children.length; i++) {
+
+        if (children[i].className.indexOf("marked") > -1) {
+            children[i].className = children[i].className.replace(/(?:^|\s)marked(?!\S)/, '');
+            avList.appendChild(children[i]);
+            //   avList.removeChild(children[i]);
+            marked = true;
+            i--;
+        }
+    }
+
+    if (!marked) {
+        alert("Ничего не выделено");
+    }
+}
+
+function Mark(listID, elemClass) {
+    var list = document.getElementById(listID),
+    elements = list.getElementsByClassName(elemClass),
+    elem;
+
+    for (var elem = 0; elem < elements.length; elem++) {
+        if (elements[elem].className.indexOf("marked") === -1) {
+            elements[elem].className += " marked";
+        }
+        else {
+            //element.classList = "";
+            elements[elem].className = elements[elem].className.replace(/(?:^|\s)marked(?!\S)/, '');
+        }
+    }
+}
+
+
+
+
---
+++ EpamTask7-3/JavaScript73.js
@@ -0,0 +1,113 @@
+﻿//var Available1 = new Array("elem1", "elem2", "elem3", "elem4");
+//var List2 = new Array("1", "2", "3", "4", "5", "6");
+//var List3 = new Array("Math", "History", "IT", "Music");
+//var List4 = new Array("elem1");
+
+//var Selected1 = new Array();
+
+function AvailableElements(availableID) {
+    
+    var availableStr = "";
+    for (var i = 0; i < availableID.length;i++)
+    {
+        availableStr += availableID [i]+ "\n";
+
+    }
+    return availableStr;
+}
+
+function AllToSelected(availableID, selectedID) {
+    var avList = document.getElementById(availableID);
+    var selList = document.getElementById(selectedID);
+    var children = avList.children;
+
+    for (var i = 0; i < children.length; i++) {
+        var option = document.createElement('li');
+        option.innerHTML = children[i].innerHTML;
+        option.id = children[i].innerHTML;
+        option.onclick = children[i].onclick;
+        selList.appendChild(option);
+    }
+
+    avList.innerHTML = "";
+}
+
+function AllToAvailable(availableID, selectedID) {
+    var avList = document.getElementById(availableID);    
+    var selList = document.getElementById(selectedID);
+    var children = selList.children;
+
+    for (var i = 0; i < children.length; i++) {
+        var option = document.createElement('li');
+        option.innerHTML = children[i].innerHTML;
+        option.id = children[i].innerHTML;
+        option.onclick = children[i].onclick;
+        avList.appendChild(option);
+    }
+    selList.innerHTML = "";
+}
+
+function ToSelected( availableID, selectedID) {
+
+        var avList = document.getElementById(availableID);  //if !exists
+        var selList = document.getElementById(selectedID);
+        var marked = false;
+        var children = avList.children;
+
+        for (var i = 0; i < children.length; i++) {
+
+            if (children[i].className.indexOf("marked") > -1) {
+                children[i].className = children[i].className.replace(/(?:^|\s)marked(?!\S)/, '');
+                selList.appendChild(children[i]);
+             //   avList.removeChild(children[i]); ???
+                marked = true;
+                i--;
+            }
+        }
+
+        if (!marked) {
+            alert("Ничего не выделено");
+        }
+}
+
+function ToAvailable(availableID, selectedID) {
+
+    var avList = document.getElementById(availableID);
+    var selList = document.getElementById(selectedID);
+    var marked = false;
+    var children = selList.children;
+
+    for (var i = 0; i < children.length; i++) {
+
+        if (children[i].className.indexOf("marked") > -1) {
+            children[i].className = children[i].className.replace(/(?:^|\s)marked(?!\S)/, '');
+            avList.appendChild(children[i]);
+            //   avList.removeChild(children[i]);
+            marked = true;
+            i--;
+        }
+    }
+
+    if (!marked) {
+        alert("Ничего не выделено");
+    }
+}
+
+function Mark(elem) {
+    var element = document.getElementById(elem);
+    
+    if (element.className == null) {
+        element.className = "marked";
+    }
+    else if (element.className.indexOf("marked")==-1) {
+        element.className += " marked";
+    }
+    else {
+        element.classList = "";
+        element.className = element.className.replace(/(?:^|\s)marked(?!\S)/, '');
+    }
+}
+
+
+
+
